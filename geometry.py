0be11f756d034c5f29096b0ce55e1f2959bf80f8
anhhuynguyen <anhhuynguyen2611@gmail.com>
2020-05-08 23:27:52 +0700

    Fix Arc.three_point()

--- geometry.py
+++ geometry.py
@@ -73,39 +73,37 @@ class Arc:
         center = np.array(center)
         start = np.array(start)
         end = np.array(end)
-        x_axis = np.array([1,0])
         
         assert Arc.distance_between(center, start) == Arc.distance_between(center, end), "Mismatched radius"
         
         self.center = center
         self.radius = Arc.distance_between(center, start)
-        self.theta_1 = Arc.angle_between(end-center, x_axis)
-        self.theta_2 = Arc.angle_between(start-center, x_axis)
+        self.theta_1 = Arc.angle_from_x_axis(end - self.center)
+        self.theta_2 = Arc.angle_from_x_axis(start - self.center)
 
     def three_point(self, first, second, third):
 
         p1 = np.array(first)
         p2 = np.array(second)
         p3 = np.array(third)
-        x_axis = np.array([1,0])
 
         a = np.linalg.det(np.array(
-            [[p1[0], p1[1], 1], [p2[0], p2[1], 1], [p3[0], p3[1], 1]
-            ]))
+            [[p1[0], p1[1], 1], [p2[0], p2[1], 1], [p3[0], p3[1], 1]]
+            ))
         bx = -np.linalg.det(np.array(
-            [[np.dot(p1,p1), p1[1], 1], [np.dot(p2,p2), p2[1], 1], [np.dot(p3,p3), p3[1], 1]
-            ]))
+            [[np.dot(p1,p1), p1[1], 1], [np.dot(p2,p2), p2[1], 1], [np.dot(p3,p3), p3[1], 1]]
+            ))
         by = np.linalg.det(np.array(
-            [[np.dot(p1,p1), p1[0], 1], [np.dot(p2,p2), p2[0], 1], [np.dot(p3,p3), p3[0], 1]
-            ]))
+            [[np.dot(p1,p1), p1[0], 1], [np.dot(p2,p2), p2[0], 1], [np.dot(p3,p3), p3[0], 1]]
+            ))
         c = -np.linalg.det(np.array(
-            [[np.dot(p1,p1), p1[0], p1[1]], [np.dot(p2,p2), p2[0], p2[1]], [np.dot(p3,p3), p3[0], p3[1]]
-            ]))
+            [[np.dot(p1,p1), p1[0], p1[1]], [np.dot(p2,p2), p2[0], p2[1]], [np.dot(p3,p3), p3[0], p3[1]]]
+            ))
 
         self.center = np.array([-bx/(2*a), -by/(2*a)])
         self.radius = np.sqrt(bx**2 + by**2 - 4*a*c) / (2*abs(a))
-        self.theta_1 = Arc.angle_between(p2 - self.center, x_axis)
-        self.theta_2 = Arc.angle_between(p1 - self.center, x_axis)
+        self.theta_1 = Arc.angle_from_x_axis(p1 - self.center)
+        self.theta_2 = Arc.angle_from_x_axis(p2 - self.center)
 
     def at(self, t):
         th = self.theta_1 * (1-t) + self.theta_2 * t
@@ -123,6 +121,9 @@ class Arc:
     def angle_between(p, q):
         return np.arccos(np.dot(p, q) / (np.linalg.norm(p) * np.linalg.norm(q)))
 
+    @staticmethod
+    def angle_from_x_axis(p):
+        return np.arctan2(p[1], p[0]) + 2*np.pi if (p[0] < 0 and p[1] < 0) else np.arctan2(p[1], p[0]) 
 
 class Point:
 
@@ -156,11 +157,9 @@ if __name__ == "__main__":
     p3 = [2,0]
     p4 = [1,0]
     p5 = [0,0]
-    m.add_Line(p1,p2)
-    m.add_Arc2(p5, p2, p3)
-    m.add_Line(p3, p4)
-    m.add_Arc2(p5, p4, p1)
+    # m.add_Line(p1, p2)
+    # m.add_Arc2(p5, p2, p3)
+    # m.add_Line(p3, p4)
+    # m.add_Arc2(p5, p4, p1)
+    m.add_Arc3([1.5,0],[0,0],[1,1])
     m.plot()
-
-
-
